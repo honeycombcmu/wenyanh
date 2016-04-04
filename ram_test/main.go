a68a43b3a72e2f615f80d73d5902c0e5776d815b
WenyanH <huwenyan15@126.com>
2016-04-04 11:26:18 -0400

    test ram memory

---
+++ ram_test/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	_ "testapi/docs"
+	_ "testapi/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
